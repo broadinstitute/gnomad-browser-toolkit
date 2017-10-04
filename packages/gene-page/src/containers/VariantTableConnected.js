796cd786d11a967571083148971d379082aaa69c
Matthew Solomonson <ggmatt@gmail.com>
2017-10-04 19:18:35 -0400

    General refactor

---
+++ packages/gene-page/src/containers/VariantTableConnected.js
@@ -0,0 +1,107 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-shadow */
+/* eslint-disable comma-dangle */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+
+import React, { PropTypes } from 'react'
+import { connect } from 'react-redux'
+import Table from '@broad/table'
+
+import {
+  activeActions,
+  variantActions,
+  tablePosition,
+  variantSearchText,
+  finalFilteredVariants,
+  finalFilteredVariantsCount,
+} from '../index'
+
+console.log(activeActions)
+
+const VariantTable = ({
+  variants,
+  setVariantSort,
+  setFocusedVariant,
+  setHoveredVariant,
+  setCurrentTableIndex,
+  setCurrentTableScrollData,
+  tablePosition,
+  searchText,
+  title,
+  height,
+  tableConfig,
+  history,
+  variantCount,
+}) => {
+  const tConfig = tableConfig(setVariantSort)
+  const scrollBarWidth = 40
+  const paddingWidth = tConfig.fields.length * 40
+  const cellContentWidth = tConfig.fields.reduce((acc, field) =>
+    acc + field.width, 0)
+  const calculatedWidth = scrollBarWidth + paddingWidth + cellContentWidth
+  return (
+    <div>
+      <Table
+        title={title}
+        height={height}
+        width={calculatedWidth}
+        tableConfig={tConfig}
+        tableData={variants}
+        remoteRowCount={variants.size}
+        loadMoreRows={() => {}}
+        overscan={5}
+        loadLookAhead={0}
+        onRowClick={setFocusedVariant(history)}
+        onRowHover={setHoveredVariant}
+        scrollToRow={tablePosition}
+        scrollCallback={setCurrentTableIndex}
+        onScroll={setCurrentTableScrollData}
+        searchText={searchText}
+      />
+    </div>
+  )
+}
+VariantTable.propTypes = {
+  variants: PropTypes.any.isRequired,
+  setVariantSort: PropTypes.func.isRequired,
+  setHoveredVariant: PropTypes.func.isRequired,
+  setFocusedVariant: PropTypes.func.isRequired,
+  setCurrentTableIndex: PropTypes.func.isRequired,
+  setCurrentTableScrollData: PropTypes.func.isRequired,
+  tablePosition: PropTypes.number.isRequired,
+  searchText: PropTypes.string.isRequired,
+  title: PropTypes.string,
+  height: PropTypes.number,
+  tableConfig: PropTypes.func.isRequired,
+  history: PropTypes.object.isRequired,
+  // setVisibleInTable: PropTypes.func.isRequired,
+}
+
+VariantTable.defaultProps = {
+  title: '',
+  height: 400,
+}
+const mapStateToProps = (state) => {
+  return {
+    variants: finalFilteredVariants(state),
+    variantCount: finalFilteredVariantsCount(state),
+    tablePosition: tablePosition(state),
+    searchText: variantSearchText(state),
+    currentNavigatorPosition: state.active.currentNavigatorPosition,
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setVariantSort: sortKey => dispatch(variantActions.setVariantSort(sortKey)),
+    setFocusedVariant: history => variantId =>
+      dispatch(variantActions.setFocusedVariant(variantId, history)),
+    setHoveredVariant: variantId => dispatch(variantActions.setHoveredVariant(variantId)),
+    setCurrentTableIndex: index => dispatch(activeActions.setCurrentTableIndex(index)),
+    setCurrentTableScrollData: scrollData =>
+      dispatch(activeActions.setCurrentTableScrollData(scrollData)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(VariantTable)
