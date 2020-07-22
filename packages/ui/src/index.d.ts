ad524945cf2cc3c260bea6a10b40a80d269cd1b4
Huy Nguyen <me@huy.dev>
2020-07-22 17:09:41 -0400

    Add TypeScript type definitions for a few of @gnomad/ui components

---
+++ packages/ui/src/index.d.ts
@@ -0,0 +1,84 @@
+import React from 'react'
+import { FixedSizeList, FixedSizeListProps } from 'react-window'
+import { StyledComponent } from 'styled-components'
+
+export interface ColumnRenderer<RowDatum, CellData> {
+  (rowDatum: RowDatum, columnKey: string, cellData: CellData): React.ReactNode
+}
+
+export interface GridPropsColumn<RowDatum, CellData> {
+  heading?: string
+  key: string
+  isRowHeader?: boolean
+  isSortable?: boolean
+  minWidth?: number
+  tooltip?: string
+  render: ColumnRenderer<RowDatum, CellData>
+}
+export interface GridProps<RowDatum, CellData> {
+  columns: GridPropsColumn<RowDatum, CellData>[]
+  cellData?: CellData
+  data: RowDatum[]
+  numRowsRendered?: number
+  onHoverRow?: (rowIndex: number) => void
+  onRequestSort?: (columnKey: string) => void
+  onScroll?: FixedSizeListProps['onScroll']
+  onVisibleRowsChange?: () => void
+  rowHeight?: number
+  rowKey?: (rowDatum: RowDatum) => string
+  shouldHighlightRow?: () => boolean
+  sortKey?: string
+  sortOrder?: 'ascending' | 'descending'
+}
+
+export class Grid<RowDatum, CellData = {}> extends React.Component<GridProps<RowDatum, CellData>> {}
+
+interface Option<OptionValue extends boolean | number | string> {
+  disabled?: boolean
+  label?: string
+  value: OptionValue
+}
+export interface SegmentedControlProps<OptionValue extends boolean | number | string> {
+  backgroundColor?: string
+  borderColor?: string
+  disabled?: boolean
+  id: string
+  onChange: (value: OptionValue) => void
+  options: Option<OptionValue>[]
+  textColor?: string
+  value: OptionValue
+}
+
+export class SegmentedControl<
+  OptionValue extends boolean | number | string
+> extends React.Component<SegmentedControlProps<OptionValue>> {}
+
+export interface CheckboxProps {
+  checked: boolean
+  disabled?: boolean
+  id: string
+  label: string
+  onChange: (isChecked: boolean) => void
+}
+
+export class Checkbox extends React.Component<CheckboxProps> {}
+
+export const Link: StyledComponent<'a', any, {}, never>
+
+interface SearchBoxItem<Value> {
+  label: string
+  value: Value
+}
+
+export interface SearchBoxProps<Value, Item extends SearchBoxItem<Value>> {
+  fetchSearchResult: (query: string) => Promise<Item[]>
+  id?: string
+  onSelect: (itemValue: Value) => void
+  placeholder?: string
+  width?: number
+}
+
+export class SearchBox<
+  Value,
+  Item extends SearchBoxItem<Value> = SearchBoxItem<Value>
+> extends React.Component<SearchBoxProps<Value, Item>> {}
