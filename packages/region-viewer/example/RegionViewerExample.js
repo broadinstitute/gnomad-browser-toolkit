e439b4ee81b085ffec3d9a3b2371a84d1ec93f4f
Nick Watts <nwatts@broadinstitute.org>
2020-12-16 10:56:18 -0500

    Change region viewer example
    * Remove transcripts track
    * Remove gnomAD query

--- packages/region-viewer/example/RegionViewerExample.js
+++ packages/region-viewer/example/RegionViewerExample.js
@@ -1,25 +1,49 @@
-import React from 'react'
-import TranscriptsTrack from '@gnomad/track-transcripts'
+import React, { useState } from 'react'
 
-import GeneViewer from './GeneViewer'
+import { RegionViewer, Track, PositionAxisTrack } from '../src'
 
-const RegionViewerExample = () => (
-  <GeneViewer geneSymbol="PCSK9" width={1000}>
-    {gene => {
-      return (
-        <TranscriptsTrack
-          activeTranscript={{
-            exons: gene.exons,
-            strand: gene.strand,
+const RegionViewerExample = () => {
+  const [position, setPosition] = useState(250)
+
+  return (
+    <div>
+      <RegionViewer
+        width={1000}
+        padding={0}
+        regions={[
+          { feature_type: 'region', start: 0, stop: 100 },
+          { feature_type: 'region', start: 200, stop: 300 },
+        ]}
+      >
+        <Track>
+          {({ scalePosition, width }) => {
+            return (
+              <svg height={50} width={width}>
+                <circle cx={scalePosition(position)} cy={25} r={10} fill="blue" />
+              </svg>
+            )
           }}
-          canonicalTranscript={gene.canonical_transcript_id}
-          transcripts={gene.transcripts}
-          showNonCodingTranscripts={false}
-          showUTRs={false}
-        />
-      )
-    }}
-  </GeneViewer>
-)
+        </Track>
+        <PositionAxisTrack />
+      </RegionViewer>
+
+      <div
+        style={{ boxSizing: 'border-box', margin: '0 auto', paddingLeft: '100px', width: '1000px' }}
+      >
+        <label htmlFor="position">
+          Position:{' '}
+          <input
+            id="position"
+            type="number"
+            value={position}
+            onChange={e => {
+              setPosition(Number(e.target.value))
+            }}
+          />
+        </label>
+      </div>
+    </div>
+  )
+}
 
 export default RegionViewerExample
