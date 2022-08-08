4e12cc435fd54be0850454aa46ef1c7d7ad23224
Phil Darnowsky <pdarnows@broadinstitute.org>
2022-08-08 16:52:33 -0400

    Decouple Markdown translation from Webpack plumbing

--- packages/markdown-loader/example/MarkdownLoaderExample.js
+++ packages/markdown-loader/example/MarkdownLoaderExample.js
@@ -1,5 +1,15 @@
-import example from './ExampleContent.md'
+import importedExample from './ExampleContent.md'
 
-document.title = example.title
+document.title = importedExample.title
 
-document.body.innerHTML = example.html
+const { translateMarkdown } = require('../src/markdownLoader')
+
+const literalSource = `
+# Begin first section
+This section was generated via \`translateMarkdown\`. The section below was \`import\`ed from a static Markdown source file.
+# End first section. Second section follows.
+`
+
+translateMarkdown(literalSource).then(translatedLiteralExample => {
+  document.body.innerHTML = [translatedLiteralExample.contents, importedExample.html].join(' ')
+})
