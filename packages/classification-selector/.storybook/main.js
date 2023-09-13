17d95219052214bd628f5d73fe1558efb9a3d24d
Phil Darnowsky <pdarnows@broadinstitute.org>
2023-09-13 15:47:52 -0400

    Upgrade to React 17
    This also makes it necessary to update a number of other packages.
    
    React 18 is out, but a number of other dependencies aren't yet compatible with it.

--- packages/classification-selector/.storybook/main.js
+++ packages/classification-selector/.storybook/main.js
@@ -1,3 +1,17 @@
+import { dirname, join } from "path";
 module.exports = {
   stories: ['../src/**/*.stories.@(ts|js)'],
+
+  framework: {
+    name: getAbsolutePath("@storybook/react-webpack5"),
+    options: {}
+  },
+
+  docs: {
+    autodocs: true
+  }
+}
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
 }
