116c729242fc75e0ffebec0e61837a4d6e014c30
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-03-27 14:45:25 -0400

    Add characterization tests for RegionViewer and friends
    This adds some tests to characterize the current behavior of RegionViewer and its subcomponents.
    
    Note that Cursor has some tests nerfed at the moment. This is becaue Cursor is currently using useRef incorrectly, in that it reads the value of said ref in the main render function, outside of an event handler or effect.

---
+++ packages/region-viewer/src/Track.spec.tsx
@@ -0,0 +1,136 @@
+import React from 'react'
+import { expect, test, describe } from '@jest/globals'
+import { render } from '@testing-library/react'
+import 'jest-styled-components'
+
+import { RegionViewer } from './RegionViewer'
+import { Track } from './Track'
+
+describe('Track', () => {
+  const TopPanelContent = ({ width, topLabel }: { width: number; topLabel: string }) => (
+    <div style={{ width }}>Top panel says "{topLabel}"</div>
+  )
+
+  const LeftPanelContent = ({ width, leftLabel }: { width: number; leftLabel: string }) => (
+    <div style={{ width }}>Left panel says "{leftLabel}"</div>
+  )
+
+  const RightPanelContent = ({ width, rightLabel }: { width: number; rightLabel: string }) => (
+    <div style={{ width }}>Right panel says "{rightLabel}"</div>
+  )
+
+  const CenterPanelContent = ({
+    leftPanelWidth,
+    regions,
+    rightPanelWidth,
+    scalePosition,
+    width,
+    centerLabel,
+  }) => (
+    <div style={{ width }}>
+      Greetings from main body of the track. "{centerLabel}"
+      <ul>
+        <li>leftPanelWidth: {leftPanelWidth}</li>
+        <li>Regions: {JSON.stringify(regions)}</li>
+        <li>rightPanelWidth: {rightPanelWidth}</li>
+        <li>scalePosition: {scalePosition(150)}</li>
+      </ul>
+    </div>
+  )
+
+  test('has no unexpected changes', () => {
+    const { asFragment } = render(
+      <RegionViewer
+        leftPanelWidth={200}
+        rightPanelWidth={110}
+        regions={[{ start: 123, stop: 456 }]}
+        width={1200}
+      >
+        <Track
+          renderLeftPanel={LeftPanelContent}
+          renderRightPanel={RightPanelContent}
+          renderTopPanel={TopPanelContent}
+          leftLabel="I am on the left"
+          rightLabel="I am on the right"
+          centerLabel="Right here in the center"
+          topLabel="Top label as in a heading or such"
+        >
+          {CenterPanelContent}
+        </Track>
+      </RegionViewer>
+    )
+
+    expect(asFragment()).toMatchSnapshot()
+  })
+
+  test('can omit any side panel', () => {
+    const { asFragment: noLeftAsFragment } = render(
+      <RegionViewer
+        leftPanelWidth={200}
+        rightPanelWidth={110}
+        regions={[{ start: 123, stop: 456 }]}
+        width={1200}
+      >
+        <Track
+          renderLeftPanel={undefined}
+          renderRightPanel={RightPanelContent}
+          renderTopPanel={TopPanelContent}
+          leftLabel="I am on the left"
+          rightLabel="I am on the right"
+          centerLabel="Right here in the center"
+          topLabel="Top label as in a heading or such"
+        >
+          {CenterPanelContent}
+        </Track>
+      </RegionViewer>
+    )
+
+    expect(noLeftAsFragment()).toMatchSnapshot()
+
+    const { asFragment: noTopAsFragment } = render(
+      <RegionViewer
+        leftPanelWidth={200}
+        rightPanelWidth={110}
+        regions={[{ start: 123, stop: 456 }]}
+        width={1200}
+      >
+        <Track
+          renderLeftPanel={LeftPanelContent}
+          renderRightPanel={RightPanelContent}
+          renderTopPanel={undefined}
+          leftLabel="I am on the left"
+          rightLabel="I am on the right"
+          centerLabel="Right here in the center"
+          topLabel="Top label as in a heading or such"
+        >
+          {CenterPanelContent}
+        </Track>
+      </RegionViewer>
+    )
+
+    expect(noTopAsFragment()).toMatchSnapshot()
+
+    const { asFragment: noRightAsFragment } = render(
+      <RegionViewer
+        leftPanelWidth={200}
+        rightPanelWidth={110}
+        regions={[{ start: 123, stop: 456 }]}
+        width={1200}
+      >
+        <Track
+          renderLeftPanel={LeftPanelContent}
+          renderRightPanel={undefined}
+          renderTopPanel={TopPanelContent}
+          leftLabel="I am on the left"
+          rightLabel="I am on the right"
+          centerLabel="Right here in the center"
+          topLabel="Top label as in a heading or such"
+        >
+          {CenterPanelContent}
+        </Track>
+      </RegionViewer>
+    )
+
+    expect(noRightAsFragment()).toMatchSnapshot()
+  })
+})
