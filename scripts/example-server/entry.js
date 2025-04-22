f30c2ad81589b9a5e3dabb520414fd2a6f13cf96
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-04-22 17:05:59 -0400

    Render example scripts using new React 18 API

--- scripts/example-server/entry.js
+++ scripts/example-server/entry.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 // scripts/example.js sets a resolver alias for this
 import ExampleComponent from 'example-component' // eslint-disable-line import/no-unresolved
 
-render(<ExampleComponent />, document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
+root.render(<ExampleComponent />)
